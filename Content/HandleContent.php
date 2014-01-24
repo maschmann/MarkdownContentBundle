835fb9863018001bff986608267b550ce25711fe
Marc Aschmann <marc@aschmann.org>
2014-01-24 20:44:29 +0100

    preparing to implement controller and views

---
+++ Content/HandleContent.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the <package> package.
+ *
+ * (c) Marc Aschmann <maschmann@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Asm\MarkdownContentBundle\Content;
+
+/**
+ * Class HandleContent
+ * @package Asm\MarkdownContentBundle\Content
+ * @author marc aschmann <maschmann@gmail.com>
+ */
+class HandleContent
+{
+    
+}
---
+++ Controller/ContentController.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the <package> package.
+ *
+ * (c) Marc Aschmann <maschmann@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Asm\MarkdownContentBundle\Controller;
+
+/**
+ * Class ContentController
+ * @package Asm\MarkdownContentBundle\Controller
+ * @author marc aschmann <maschmann@gmail.com>
+ */
+class ContentController
+{
+    public function indexAction()
+    {
+
+    }
+}
