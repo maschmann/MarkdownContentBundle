adf9c7278e02972093e8eff9e4200cc5a1e538be
Marc Aschmann <marc@aschmann.org>
2014-01-26 21:10:53 +0100

    added basic route handling in controller

--- Controller/ContentController.php
+++ Controller/ContentController.php
@@ -23,10 +23,23 @@ class ContentController extends Controller
 {
     public function indexAction($page)
     {
+        //$provider = $this->get('asm_markdown_content.content_provider');
+        $content = array(
+            'data' => array(
+                'charset'     => 'utf-8',
+                'title'       => 'testing title',
+                'description' => 'testing description',
+                'author'      => 'marc aschmann',
+            ),
+            'content' => 'XxxXXXxxxxxXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxXXXXXXXXXXXXXXXXXx',
+        );
+
         return $this->render(
             'AsmMarkdownContentBundle:Content:index.html.twig',
             array(
-                'translations' => $translations,
+                'data'     => $content['data'],
+                'content'  => $content['content'],
+                'language' => $this->get('request')->getLocale(),
             )
         );
     }
